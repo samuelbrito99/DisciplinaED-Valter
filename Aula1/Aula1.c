9762a6e4fb5b8c2a8547704931bba34f235c3405
RaimundoValter <valter.costa@ifce.edu.br>
2025-03-20 21:55:26 -0300

    Aula 2

--- Aula1/Aula1.c
+++ Aula1/Aula1.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 
+double variavel1 = 65536;
+
+void imprime(void);
+
 int main(){
 
-    printf("Olá mundo!");
-     
+
+    printf("Bytes usados pela variavel1: %d\n", sizeof(variavel1));
+    printf("Endereço: %x\n", &variavel1);
+    printf("Valor: %f\n", variavel1);
+    imprime();
+
     return 0;
+}
+
+void imprime(){
+    int variavel2 = 1000;
+    printf("Endereço: %x\n", &variavel2);
+    printf("Valor variável2: %d\n", variavel2);   
 }
